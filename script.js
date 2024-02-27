d817ffe54dbde446558e25652525b59f71290f4a
Pavlo Zhabort <zhabortp@gmail.com>
2024-02-27 19:46:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const firstNumber = +prompt("Введіть перше число: ");
+const secondNumber = +prompt("Введіть друге число: ");
+const userAction = prompt('Що ви хочете зробити (add, sub, mult, div) ?');
+
+let result;
+
+if (userAction === 'add' || userAction === '+') {
+    result = firstNumber + secondNumber;
+    alert(`${firstNumber} + ${secondNumber} = ${result}`)
+} else if (userAction === 'sub' || userAction === '-') {
+    result = firstNumber - secondNumber;
+    alert(`${firstNumber} - ${secondNumber} = ${result}`)
+} else if (userAction === 'mult' || userAction === '*') {
+    result = firstNumber * secondNumber;
+    alert(`${firstNumber} * ${secondNumber} = ${result}`)
+} else if (userAction === 'div' || userAction === '/') {
+    result = firstNumber / secondNumber;
+    alert(`${firstNumber} / ${secondNumber} = ${result}`)
+} else {
+    alert('Numbers pls')
+}
